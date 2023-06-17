fb096d4dee033db038cd85e07fa5e6f4cfbd78cb
itsyoman <yordiasfaw9@gmail.com>
2023-06-17 05:32:13 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,9 +4,16 @@
 
 #define STDERR_FILENO 2
 
+/**
+ * main - Entry point
+ *
+ * Description: Prints a message to the standard error output.
+ *
+ * Return: Always 1 (Exit failure)
+ */
 int main(void)
 {
-const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-syscall(SYS_write, STDERR_FILENO, message, strlen(message));
-return (1);
+    const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+    syscall(SYS_write, STDERR_FILENO, message, strlen(message));
+    return (1);
 }
